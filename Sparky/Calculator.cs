7595f3c31e1f983b17c301b6984fcfc0706f2a92
Ford Henley <info@fordswebhelp.com>
2026-03-03 13:42:22 -0500

    Created test to check two inputs with doubles

--- Sparky/Calculator.cs
+++ Sparky/Calculator.cs
@@ -7,6 +7,11 @@
             return a + b;
         }
 
+        public double AddNumberDouble(double a, double b)
+        {
+            return a + b;
+        }
+
         public bool IsOddNumber(int a)
         {
             return a % 2 != 0;
