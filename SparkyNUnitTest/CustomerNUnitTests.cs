7963033d6107ba506b5596091ca180af5e93d231
Ford Henley <info@fordswebhelp.com>
2026-03-03 15:49:03 -0500

    Completed customer tests and added Setup constructor

--- SparkyNUnitTest/CustomerNUnitTests.cs
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -1,28 +1,53 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sparky
+namespace Sparky 
 {
     [TestFixture]
     public class CustomerNUnitTests
     {
+        private Customer customer;
+        [SetUp]
+        public void Setup()
+        {
+            customer = new Customer();
+        }
+
         [Test]
-        [TestCase("John", "Doe")]
         [TestCase("Ben", "Spark")]
         public void CombineName_InputFirstAndLastName_ReturnFullName(string firstName, string lastName)
         {
             // Arrange
-            var customer = new Customer();
+            
 
             // Act
             string fullName = customer.GreetAndCombineNames(firstName, lastName);
 
             // Assert
+            //ClassicAssert.AreEqual(fullName, "Hello, Ben Spark");
             Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+            Assert.That(fullName, Does.Contain("ben Spark").IgnoreCase);
+            Assert.That(fullName, Does.StartWith("Hello,"));
+            Assert.That(fullName, Does.EndWith("Spark"));
+            Assert.That(fullName, Does.Match("Hello, [A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+"));
+
+        }
+
+        [Test]
+        public void GreetMessage_NotGreeted_ReturnNull()
+        {
+            // Arrange
+            
+
+            // Act
+
+            // Assert
+            ClassicAssert.IsNull(customer.GreetMessage);
         }
     }
 }
