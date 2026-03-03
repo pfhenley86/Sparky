a93138915910e95781f7c9c2035431fd4b4f718d
Ford Henley <info@fordswebhelp.com>
2026-03-03 11:35:14 -0500

    Created test project and class

---
+++ SparkyMSTest/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyMSTest
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
