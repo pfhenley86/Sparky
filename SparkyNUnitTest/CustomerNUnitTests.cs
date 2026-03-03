49a50ca4a0d8df5fd95f12fc98a92a531c664568
Ford Henley <info@fordswebhelp.com>
2026-03-03 13:54:18 -0500

    Created Customer Class and started Customer Testcases

---
+++ SparkyNUnitTest/CustomerNUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CustomerNUnitTests
+    {
+        [Test]
+        [TestCase("John", "Doe")]
+        [TestCase("Ben", "Spark")]
+        public void CombineName_InputFirstAndLastName_ReturnFullName(string firstName, string lastName)
+        {
+            // Arrange
+            var customer = new Customer();
+
+            // Act
+            string fullName = customer.GreetAndCombineNames(firstName, lastName);
+
+            // Assert
+            Assert.That(fullName, Is.EqualTo("Hello, Ben Spark"));
+        }
+    }
+}
