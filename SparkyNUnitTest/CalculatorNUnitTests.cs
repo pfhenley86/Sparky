a65829617c127d94693273e629bdc1043a644947
Ford Henley <info@fordswebhelp.com>
2026-03-03 12:59:35 -0500

    Created Odd Number method and tests

---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            int result = calc.AddNumber(10, 20);
+
+            //Assert
+           ClassicAssert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void IsOddChecker_InputEvenNumber_ReturnFalse()
+        {
+            //Arrange
+            Calculator calc = new ();
+
+            //Act
+            bool isOdd = calc.IsOddNumber(10);
+
+            //Assert
+            ClassicAssert.That(isOdd,Is.EqualTo(false));
+            ClassicAssert.IsFalse(isOdd);
+        }
+
+        [Test]
+        public void IsOddChecker_InputOddNumber_ReturnTrue()
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            bool isOdd = calc.IsOddNumber(11);
+
+            //Assert
+            ClassicAssert.That(isOdd, Is.EqualTo(true));
+            ClassicAssert.IsTrue(isOdd);
+        }
+    }
+}
